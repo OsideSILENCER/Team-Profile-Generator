99cf778cdab78bb085b03982f7deb2524a7d0055
Phillip Stevenson <phillipestevenson@yahoo.com>
2022-04-25 23:38:08 -0700

    All updates

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Team-Profile-Generator
+
+## Description
+ This is a README generator server.
+
+I was motivated by my instructor John to build this.
+
+I built this project utilizing html, multiple javascript pages and even gitignore.
+I had issues connecting the different variables at first.
+It solves the issue of getting the project to work
+I learned how to create README.md
+
+## Installation
+
+In order to install this README.md you must first answer the input questons and then it will ato populate by itself.
+
+## Usage
+
+In order to use this generator simply enter in your answers into the input and it will auto populate itself.
+
+## Credits
+
+N/A
---
+++ index.js
@@ -0,0 +1,124 @@
+const inquirer = require("inquirer");
+const  generateTeam = require("./generateMarkdown");
+const fs = require("fs");
+const { type } = require("os");
+
+
+const Engineer = ("./lib/Engineer");
+const Intern = ("./lib/Intern");
+const Manager = ("./lib/Manager");
+
+const newStaffMemberData = [];
+
+const questions = async () => {
+    const answers = await inquirer
+inquirer.prompt([
+    {
+        input: "input",
+        message: "What is your name?",
+        name: "name",
+    },
+    {
+        input: "input",
+        message: "What is your id number?",
+        name: "id",
+    },
+    {
+        input: "input",
+        message: "Why is your email?",
+        name: "email",
+    },
+    {
+        input: "List",
+        message: "What is your role?",
+        choices: ["Engineer", "Intern", "Manager"],
+    },
+    ])
+
+    if (answers.role === "Manager") {
+        const managerAns = await inquirer
+        .prompt([
+        {
+            input: "input",
+            message: "What is your office number?",
+            name: "officeNumber",
+        },
+        ])
+        const newManager = new Manager (
+            answers.name,
+            answers.id,
+            answers.email,
+            managerAns.officeNumber
+        );
+        newStaffMemberData.push(newManager);
+        }
+        else if (answers.role === "Engineer") {
+            const githubAns = await inquirer
+            .prompt([
+                {
+                    input: "input",
+                    message: "What is your GitHub user name?",
+                    name: "github",
+                },
+            ])
+            const newEngineer = new Engineer (
+                answers.name,
+                answers.id,
+                answers.email,
+                githubAns.github
+            );
+            newStaffMemberData.push(newEngineer);
+        }
+     else if (answers.role === "Intern") {
+        const InternAns = await inquirer
+        .prompt([
+            {
+                input: "input",
+                message: "What university did you attend?",
+                name: "school",
+            },
+        ])
+        const newIntern = new Intern(
+            answers.name,
+            answers.id,
+            answers.email,
+            InternAns.school
+        );
+        newStaffMemberData.push(newIntern);
+    };
+    
+    async function promptQuestions() {
+        await questions()
+
+        const addMemberAns = await inquirer
+        .prompt([
+            {
+                name: "addMember",
+                type: "List",
+                choices: ["Add a new member", "Create team"],
+                message: "Whaht would you like to do next?",
+            },
+        ])
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+.then((answers) => {
+    generateMarkdown(answers);
+    console.log(answers)
+});
+}
